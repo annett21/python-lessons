9c830766c3a3c375dfb1356092b3676848de8903
annett21 <annettarasevich@gmail.com>
2022-04-06 13:41:45 +0400

    resolved third task

--- module14/03_sum_and_dif/main.py
+++ module14/03_sum_and_dif/main.py
@@ -1,1 +1,12 @@
+num = int(input('Введите число: '))
 
+def sum_digits(num):
+    return sum(map(int, str(num)))
+
+def num_of_digits(num):
+    return len(str(num))
+
+print('Сумма цифр:', sum_digits(num))
+print('Кол-во цифр в числе:', num_of_digits(num))
+
+print('Разность суммы и кол-ва цифр:', sum_digits(num) - num_of_digits(num))
