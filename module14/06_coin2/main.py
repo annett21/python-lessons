abfd5b93fb633b2c753f14cc97a21e7c29ab130b
annett21 <annettarasevich@gmail.com>
2022-04-06 16:15:54 +0400

    resolved 6th task

--- module14/06_coin2/main.py
+++ module14/06_coin2/main.py
@@ -1,1 +1,16 @@
 
+print('Введите координаты монетки: ')
+
+x = float(input('X: '))
+y = float(input('Y: '))
+
+r = int(input('Введите радиус: '))
+
+def check_coin(x, y, r):
+    c = x ** 2 + y ** 2
+    return c <= r ** 2
+   
+if check_coin(x, y, r):
+    print('Монетка где-то рядом')
+else:
+    print('Монетки в области нет')
