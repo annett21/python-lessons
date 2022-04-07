f076081d85c06185fd4688a95b448cc78c57a098
annett21 <annettarasevich@gmail.com>
2022-04-07 14:05:33 +0400

    resolves 2nd task

--- modul15/homework/task_02_tournament/main.py
+++ modul15/homework/task_02_tournament/main.py
@@ -5,10 +5,7 @@ def display_result(participants_names):
     :param participants_names: список имён участников, например: ["Артемий", "Влад", "Дима", "Женя"]
     :type participants_names: List[str]
     """
-    # TODO: в этой функции пишем весь необходимый код 
-    #  для вывода результата в нужном формате.
-    #  Логику расчётов тут не программируем
-    pass
+    print("Первый день:", participants_names)
 
 
 def get_participants_names(names):
@@ -21,19 +18,16 @@ def get_participants_names(names):
     :return: список имён с чётными индексами , например: ["Артемий", "Влад", "Дима", "Женя"]
     :rtype: List[str]
     """
-    # TODO: в этой функции получаем элементы списка только с чётными индексами..
-    #  print'ов и input'ов тут не должно быть. 
-    #  Функция на вход принимает ранее полученные данные
-    #  (из функции get_input_parameters).
-    #  Функция на выход отдаёт результат необходимый для отображения работы программы,
-    #  который будет передан в функцию display_result.
-    pass
+    participants_names = []
+    for i in range(0, len(names), 2):
+        participants_names.append(names[i])
+
+    return participants_names
+
 
 
 if __name__ == '__main__':
-    # Это условие необходимо, чтобы в рамках автотестов не произошёл
-    # вызов функций get_input_parameters и display_result
     participants_names = get_participants_names(
         ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
-    )  # получаем список имён с чётными индексами
-    display_result(participants_names)  # выводим результат
+        )
+    display_result(participants_names)
