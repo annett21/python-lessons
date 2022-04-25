ad4f68c400e4811e65d7526e0f02197553f0c3e5
annett21 <annettarasevich@gmail.com>
2022-04-25 21:04:52 +0300

    resolved 4th task

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,31 +1,20 @@
 guests = ["Peter", "Ivan", "Alex", "Lisa", "Kate"]
 
 while True:
-    print(f"There are {len(guests)} guests at the party:", guests)
-
-    user_input = input("Has a guest come or left? (come/left/time to sleep) ")
-
-    if user_input not in ("come", "left", "time to sleep"):
-        continue
-
-    if user_input == "time to sleep":
-        print("The party is over.")
-        break
-
-    guest_name = input("Name of guest: ")
-
-    if user_input == "left":
-        if guest_name not in guests:
-            print(f"There is no {guest_name} here.")
-            continue
-
-        guests.remove(guest_name)
-        print(f"Bye, {guest_name}")
-
-    elif user_input == "come":
+    print(f"There are {len(guests)} friends at the party right now:", guests)
+    answer = input("Have the guest come or left? ")
+    if answer == "come":
         if len(guests) >= 6:
-            print(f"Sorry, {guest_name}, but there is no place.")
-            continue
-
-        guests.append(guest_name)
-        print(f"Hi, {guest_name}")
+            name = input("Guest name: ")
+            print(f"Sorry, {name}, we're full.")
+        elif len(guests) < 6:
+            name = input("Guest name: ")
+            guests.append(name)
+            print(f"Hello, {name}!")
+    elif answer == "left":
+        name = input("Guest name: ")
+        print(f"Bye, {name}!")
+        guests.remove(name)
+    elif answer == "sleep time":
+        print("The party is over, everybody is sleeping.")
+        break
