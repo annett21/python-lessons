0e0037b1b4a27b31d328d98371555f36f7116f77
annett21 <annettarasevich@gmail.com>
2022-04-26 13:12:42 +0300

    resolved 8th task

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -2,26 +2,16 @@ people_quantity = int(input("Количество человек: "))
 step = int(input("Какое число в считалке? "))
 print(f"Значит, выбывает каждый {step} человек.")
 
-people = list(range(1, people_quantity + 1))
+circle = list(range(1, people_quantity + 1))
+count = 0
 
-
-def run_count(start_index=0):
-    if len(people) == 1:
-        return people[0]
-
-    start_index = 0 if start_index == len(people) else start_index
-
-    print("\nТекущий круг людей:", people)
-    print("Начало счёта с номера", people[start_index])
-
-    normalized_step = step % len(people)
-    leaving_index = (start_index + normalized_step - 1) % len(people)
-    print("Выбывает человек под номером", people[leaving_index])
-    del people[leaving_index]
-
-    return run_count(leaving_index)
-
-
-remaining_person = run_count()
-
-print("\nОстался человек под номером", remaining_person)
+while len(circle) > 1:
+    print("Текущий круг людей", circle)
+    print(f"Начало счета с номера {circle[count]}")
+    count = (count + step - 1) % len(circle)
+    if circle[count] == circle[-1]:
+        print(f"Выбывает человек под номером {circle.pop(count)}")
+        count = 0
+    else:
+        print(f"Выбывает человек под номером {circle.pop(count)}")
+print("Остался человек под номером", circle[0])
