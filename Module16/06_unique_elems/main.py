e54316356e38fc43f3ef93721d6ab5af13128720
annett21 <annettarasevich@gmail.com>
2022-04-26 22:02:57 +0300

    resilved 6th task

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,28 +1,18 @@
-from random import randint
+from collections import Counter
 
+base_list = []
+side_list = []
 
-def create_and_fill_list(length: int, list_name: str) -> list[int]:
-    new_list = []
+for num in range(1, 3 + 1):
+    base_num = int(input(f"Enter {num} for the base list: "))
+    base_list.append(base_num)
 
-    for number in range(1, length + 1):
-        user_input = input(f"Enter the {number} number for {list_name} list: ")
-        try:
-            new_list.append(int(user_input))
-        except ValueError:
-            new_list.append(randint(1, 10))
+for numb in range(1, 7 + 1):
+    side_num = int(input(f"Enter {numb} for the side list: "))
+    side_list.append(side_num)
 
-    return new_list
+print("Base list:", base_list)
+print("Side list", side_list)
 
-
-first_list = create_and_fill_list(3, "first")
-second_list = create_and_fill_list(7, "second")
-
-print("\nThe first list:", first_list)
-print("The second list:", second_list)
-
-first_list.extend(second_list)
-for number in first_list:
-    while first_list.count(number) > 1:
-        first_list.remove(number)
-
-print("\nThe new first list:", first_list)
+base_list.extend(side_list)
+print("Updated base list with unique elements", list(Counter(base_list)))
