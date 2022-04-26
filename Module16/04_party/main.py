e4cb196b2dd93cad8553fb645cd9631ca491f344
annett21 <annettarasevich@gmail.com>
2022-04-26 10:32:26 +0300

    improved 4th task

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -3,18 +3,16 @@ guests = ["Peter", "Ivan", "Alex", "Lisa", "Kate"]
 while True:
     print(f"There are {len(guests)} friends at the party right now:", guests)
     answer = input("Have the guest come or left? ")
+    if answer == "sleep time":
+        print("The party is over, everybody is sleeping.")
+        break
+    name = input("Guest name: ")
     if answer == "come":
         if len(guests) >= 6:
-            name = input("Guest name: ")
             print(f"Sorry, {name}, we're full.")
         elif len(guests) < 6:
-            name = input("Guest name: ")
             guests.append(name)
             print(f"Hello, {name}!")
     elif answer == "left":
-        name = input("Guest name: ")
         print(f"Bye, {name}!")
         guests.remove(name)
-    elif answer == "sleep time":
-        print("The party is over, everybody is sleeping.")
-        break
