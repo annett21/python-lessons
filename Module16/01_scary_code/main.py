fd23f7db5f078c7831c6d6911e018f007884348f
annett21 <annettarasevich@gmail.com>
2022-04-26 10:18:26 +0300

    improved 1st task

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -5,9 +5,13 @@ second_list = [1, 3, 1, 5, 3, 3]
 main_list.extend(first_list)
 print("Кол-во цифр 5 при первом объединении:", main_list.count(5))
 
-for i in main_list:
-    if i == 5:
-        main_list.remove(i)
+# for i in main_list:
+#     if i == 5:
+#         main_list.remove(i)
+
+while 5 in main_list:
+    main_list.remove(5)
+
 
 main_list.extend(second_list)
 print("Кол-во цифр 3 при втором объединении:", main_list.count(3))
