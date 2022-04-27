b3063151c0b1dd2550060d2c58b9a339689d519f
annett21 <annettarasevich@gmail.com>
2022-04-27 19:07:37 +0300

    improved variable name in 8th task

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -3,15 +3,15 @@ step = int(input("Какое число в считалке? "))
 print(f"Значит, выбывает каждый {step} человек.")
 
 circle = list(range(1, people_quantity + 1))
-count = 0
+current_person = 0
 
 while len(circle) > 1:
     print("Текущий круг людей", circle)
-    print(f"Начало счета с номера {circle[count]}")
-    count = (count + step - 1) % len(circle)
-    if circle[count] == circle[-1]:
-        print(f"Выбывает человек под номером {circle.pop(count)}")
-        count = 0
+    print(f"Начало счета с номера {circle[current_person]}")
+    current_person = (current_person + step - 1) % len(circle)
+    if circle[current_person] == circle[-1]:
+        print(f"Выбывает человек под номером {circle.pop(current_person)}")
+        current_person = 0
     else:
-        print(f"Выбывает человек под номером {circle.pop(count)}")
+        print(f"Выбывает человек под номером {circle.pop(current_person)}")
 print("Остался человек под номером", circle[0])
