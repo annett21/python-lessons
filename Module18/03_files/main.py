0ef1662af26ac1ddf1c7595fd5507d9ab34e07f7
annett21 <annettarasevich@gmail.com>
2022-05-13 11:32:36 +0300

    re-resolved 3rd task

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,23 +1,11 @@
-from tabnanny import check
-
-
-def check_char(text, chars):
-    for char in chars:
-        if text.startswith(char):
-            return True
-
-
-wrong_char = "@№$%^&\*()"
+wrong_char = tuple("@№$%^&\*()")
 type_file = (".txt", ".doxc")
 
 name = input("Your file name: ")
 
-if not check_char(name, wrong_char) and name.endswith(type_file):
+if name.startswith(wrong_char):
+    print("Error: file name starts with one of special char")
+if not name.endswith(type_file):
+    print(f"Error: wrong file type. Expected {type_file[0]} or {type_file[1]}.")
+if not name.startswith(wrong_char) and name.endswith(type_file):
     print("File name is correct")
-elif not check_char(name, wrong_char) and not name.endswith(type_file):
-    print(f"Error: wrong file type. Expected {type_file[0]} or {type_file[1]}.")
-elif check_char(name, wrong_char) and name.endswith(type_file):
-    print("Error: file name starts with one of special char")
-else:
-    print("Error: file name starts with one of special char")
-    print(f"Error: wrong file type. Expected {type_file[0]} or {type_file[1]}.")
