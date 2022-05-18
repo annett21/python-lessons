6f30f8eb6a08b8430ca8c27ecbb876540af63241
annett21 <annettarasevich@gmail.com>
2022-05-18 18:02:24 +0300

    resolved 8th task

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+max_number = int(input("Введите максимальное число: "))
+yes_set = set()
+no_set = set()
+
+num = input("Нужное число есть среди вот этих чисел: ")
+
+while num != "help":
+    answer = input("Ответ Артёма: ")
+    if answer == "Да" or answer == "yes":
+        yes_set = set(num.split())
+    elif answer == "Нет" or answer == "no":
+        no_set = set(num.split())
+    num = input("Нужное число есть среди вот этих чисел: ")
+
+
+print("Артём мог загадать следующие числа:", yes_set.difference(no_set))
