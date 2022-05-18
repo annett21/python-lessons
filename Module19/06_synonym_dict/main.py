df3af30d0b1b2564eb95e0408410f70a71166d7e
annett21 <annettarasevich@gmail.com>
2022-05-18 14:50:13 +0300

    resolved 6th task

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+num = int(input("Введите количество пар слов: "))
+synonyms = {}
+
+for i in range(1, num + 1):
+    word_pair = input(f"{i} пара: ")
+    words = word_pair.split(" - ")
+    synonyms[words[0]] = words[1]
+
+
+def find_synonyms(word):
+    word = word.lower()
+    for key, value in synonyms.items():
+        if word == key.lower():
+            return value
+        elif word == value.lower():
+            return key
+
+
+word_check = input("Введите слово: ")
+
+while not find_synonyms(word_check):
+    print("Такого слова в словаре нет.")
+    word_check = input("Введите слово: ")
+
+print("Синоним:", find_synonyms(word_check))
