861a3691c793c54bad9d2f3c4080277f1acb5e39
annett21 <annettarasevich@gmail.com>
2022-05-20 13:09:53 +0300

    half-solved 9th task

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+# num = int(input("Введите количество человек: "))
+
+# names = {}
+
+# for i in range(1, num):
+#     name = input(f"{i} пара: ").split()
+#     if name[1] not in names:
+#         names[name[1]] = []
+#     names[name[1]].append(name[0])
+
+# print(names)
+
+names = {
+    "Peter_I": ["Alexei", "Anna", "Elizabeth"],
+    "Alexei": ["Peter_II"],
+    "Anna": ["Peter_III"],
+    "Peter_III": ["Paul_I"],
+    "Paul_I": ["Alexander_I", "Nicholaus_I"],
+}
+
+# print("Высота» каждого члена семьи:", )
