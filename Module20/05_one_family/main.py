7083c1017ef0f7dbab20ae83b6e90d9413b1f493
annett21 <annettarasevich@gmail.com>
2022-05-23 20:40:39 +0300

    resolved 5th task

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+def is_women(string):
+    return string[-1] == "а"
+
+
+family = {
+    ("Сидоров", "Никита"): 35,
+    ("Сидорова", "Алина"): 34,
+    ("Сидоров", "Павел"): 10,
+    ("Иванов", "Дмитрий"): 15,
+    ("Иванова", "Алиса"): 18,
+    ("Смирнов", "Кирилл"): 25,
+}
+
+surname_search = input("Введите фамилию: ").capitalize()
+print()
+
+
+for (surname, name), ages in family.items():
+    if surname == surname_search:
+        print(surname, name, ages)
+    elif is_women(surname):
+        if surname[:-1] == surname_search:
+            print(surname, name, ages)
+    elif is_women(surname_search):
+        if surname == surname_search[:-1]:
+            print(surname, name, ages)
