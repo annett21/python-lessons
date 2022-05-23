1fbe8640716d250b78dfb75e56392e83ff97d9d1
annett21 <annettarasevich@gmail.com>
2022-05-23 13:59:39 +0300

    resolved 2nd task

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def is_prime(num):
+    if num == 0:
+        return False
+    for i in range(2, (num // 2) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def crypto(iterable_object):
+    return [
+        element
+        for index, element in enumerate(iterable_object)
+        if is_prime(index)
+    ]
+
+
+print(crypto([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(crypto("О Дивный Новый мир!"))
