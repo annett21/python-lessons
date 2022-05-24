bbca6151a8a3bb8d52fa2878e409a43419680756
annett21 <annettarasevich@gmail.com>
2022-05-24 11:20:42 +0300

    resolved 6th task

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+original_list = list(range(10))
+
+print("Оригинальный список:", original_list)
+
+new_list = [(x, x + 1) for x in range(0, 10, 2)]
+
+# new_list = [(x, x + 1) for num in original_list for x in range(0, num, 2)]
+
+print("Новый список:", new_list)
