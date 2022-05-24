894292bef13e651b70c63a059622ede19c7394ff
annett21 <annettarasevich@gmail.com>
2022-05-24 11:31:52 +0300

    resolved 7th task

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def tpl_sort(any_tuple):
+    for element in any_tuple:
+        if not isinstance(element, int):
+            return any_tuple
+    return sorted(any_tuple)
+
+
+print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
