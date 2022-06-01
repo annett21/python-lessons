3837a9269db2eb04fa12621ade6848fb1b95fa62
annett21 <annettarasevich@gmail.com>
2022-06-01 14:02:59 +0300

    update on 10th task from m-20 / part of 2nd sol.

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+from typing import Iterable
+
+
+def new_zip(a: Iterable, b: Iterable, i=0) -> None:
+    print((a[i], b[i]))
+    if i == (len(a) - 1):
+        return
+    new_zip(a, b, i + 1)
+
+
+new_zip([1, 2, 3], "abc")
