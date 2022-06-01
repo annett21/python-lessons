f4229439c25e1b17a40828801d5747b6062c4061
annett21 <annettarasevich@gmail.com>
2022-06-01 13:34:32 +0300

    resolved 1st task

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+num = int(input("Введите num: "))
+
+
+def output_numbers(num: int) -> None:
+    if num != 1:
+        output_numbers(num - 1)
+    print(num)
+
+
+def output_numbers_2(num: int, current_num: int = 1) -> None:
+    print(current_num)
+    if current_num == num:
+        return
+    output_numbers_2(num, current_num + 1)
+
+
+output_numbers(num)
+print("--__--")
+output_numbers_2(num)
