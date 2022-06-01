297ccea4211660fad6b57af5990cdf726a63368b
annett21 <annettarasevich@gmail.com>
2022-06-01 12:46:37 +0300

    improved 8th task solution

--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,36 +1,31 @@
-def find_contact(d, contact):
-    for (_, surname) in d:
-        if contact == surname:
-            return True
+def add_contact(contacts: dict):
+    name, surname = input(
+        "Введите имя и фамилию нового контакта (через пробел): "
+    ).split()
+    if (name, surname) in contacts:
+        print("Такой человек уже есть в контактах.")
+    else:
+        phone_number = int(input("Введите номер телефона: "))
+        contacts[(name, surname)] = phone_number
+    print("Текущий словарь контактов: ", contacts)
 
 
-def add_contact(d, name, surname, phone):
-    d[(name, surname)] = phone
+def find_contacts(contacts: dict):
+    search_surname = input("Введите фамилию для поиска: ").capitalize()
+    for (name, surname) in contacts:
+        if search_surname == surname:
+            print(name, surname, contacts[(name, surname)])
 
 
 contacts = {}
 action = 0
+text = (
+    "Введите номер действия: 1. Добавить контакт 2. Найти человека 3. Выход\n"
+)
 
 while action != 3:
-    action = int(
-        input(
-            "Введите номер действия: 1. Добавить контакт 2. Найти человека"
-            " 3. Выход\n"
-        )
-    )
+    action = int(input(text))
     if action == 1:
-        name, surname = input(
-            "Введите имя и фамилию нового контакта (через пробел): "
-        ).split()
-        if find_contact(contacts, surname):
-            print("Такой человек уже есть в контактах.")
-        else:
-            phone_number = int(input("Введите номер телефона: "))
-            add_contact(contacts, name, surname, phone_number)
-        print("Текущий словарь контактов: ", contacts)
-
-    if action == 2:
-        search_surname = input("Введите фамилию для поиска: ").capitalize()
-        for (name, surname) in contacts:
-            if search_surname == surname:
-                print(name, surname, contacts[(name, surname)])
+        add_contact(contacts)
+    elif action == 2:
+        find_contacts(contacts)
