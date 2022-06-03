11c599cb564bd0de634a8b8961f1285e9b775f5e
annett21 <annettarasevich@gmail.com>
2022-06-03 12:41:22 +0300

    improved 2nd task solution (check out=)

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,11 +1,25 @@
 from typing import Iterable
 
 
-def new_zip(a: Iterable, b: Iterable, i=0) -> None:
-    print((a[i], b[i]))
-    if i == (len(a) - 1):
+# def new_zip(a: Iterable, b: Iterable, i: int = 0) -> None:
+#     print((a[i], b[i]))
+#     if i == (len(a) - 1):
+#         return
+#     new_zip(a, b, i + 1)
+
+
+# new_zip([1, 2, 3], "abc")
+
+
+def new_zip_2(x: Iterable, y: Iterable, file: list = [], i: int = 0) -> list:
+    file.append((x[i], y[i]))
+    if len(file) == len(x):
         return
-    new_zip(a, b, i + 1)
+    new_zip_2(x, y, file, i + 1)
+    return file
 
 
-new_zip([1, 2, 3], "abc")
+print(new_zip_2([1, 2, 3, 4], "abcd"))
+
+# попробуй вместо принта сохранять кортежи в список и передавать
+#  этот список через весь стек вызовов как i, и возвращать этот список
