dc6241a1094f96351ef9fc18c42146974ea6f57e
annett21 <annettarasevich@gmail.com>
2022-06-03 13:23:15 +0300

    resolved 5th task

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,15 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
-    result = result ** 10
+def recur_factorial(n: int) -> int:
+    if n == 1:
+        return n
+    else:
+        return n * recur_factorial(n - 1)
+
+
+def calculating_math_func(data: int, recur_factorial: int) -> int:
+    result = recur_factorial(data)
+    result /= data**3
+    result = result**10
     return result
 
-# TODO оптимизировать функцию
+
+print(calculating_math_func(5, recur_factorial))
