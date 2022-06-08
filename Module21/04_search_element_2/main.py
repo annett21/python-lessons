ab1ff4a3cd728c416fd32a786fa8d3a29a2c8048
annett21 <annettarasevich@gmail.com>
2022-06-08 16:02:23 +0300

    added minor changes 4th task

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-
 site = {
     "html": {
         "head": {"title": "Мой сайт"},
@@ -12,25 +11,27 @@ site = {
     }
 }
 
-search_key = input("Введите искомый ключ: ").lower()
-search_parametr = input("Хотите ввести максимальную глубину? y/n: ")
-if search_parametr == "y":
-    depth = input("Введите максимальную глубину: ")
 
-
-def find_key(data: dict, key: str) -> Any:
+def find_key(data: dict, key: str, i: int = 1) -> Any:
     if key in data:
         return data[key]
 
     for i_data in data.values():
         if isinstance(i_data, dict):
+            i += 1
             result = find_key(i_data, key)
             if result:
                 break
     else:
         result = None
 
-    return result
+    return result, i
 
+
+search_key = input("Введите искомый ключ: ").lower()
+search_parametr = input("Хотите ввести максимальную глубину? y/n: ")
+if search_parametr == "y":
+    depth = input("Введите максимальную глубину: ")
+    # print(find_key(site, search_key, depth))
 
 print(find_key(site, search_key))
