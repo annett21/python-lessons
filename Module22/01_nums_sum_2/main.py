d1162ec0aff56beac30b0188aae19e99ac57c8b9
annett21 <annettarasevich@gmail.com>
2022-06-16 14:13:58 +0300

    1st task solution

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def get_numbers_from_file(file_path):
+    with open(file_path, "r") as numbers:
+        return [int(i_num.strip()) for i_num in numbers if i_num.strip()]
+
+
+def write_sum_to_file(file_path, numbers):
+    with open(file_path, "a") as answer:
+        answer.write(str(sum(numbers)) + "\n")
+
+
+absolute_path = "/Users/anatarasevic/python_lessons/Module22/01_nums_sum_2/"
+answer_path = absolute_path + "answer.txt"
+number_path = absolute_path + "numbers.txt"
+
+write_sum_to_file(answer_path, get_numbers_from_file(number_path))
