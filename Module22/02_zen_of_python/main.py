015e65e2fce8d6b0b1b9eb234c2bfc3148e367eb
annett21 <annettarasevich@gmail.com>
2022-06-16 15:00:34 +0300

    resolved 2nd task

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+def reverse_file_text(file_path):
+    with open(file_path, "r") as text:
+        for i_text in reversed(text.readlines()):
+            print(i_text, end="")
+
+
+reverse_file_text("Module22/02_zen_of_python/zen.txt")
