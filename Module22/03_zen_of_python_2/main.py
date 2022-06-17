888be68ef94537f76107b00d44df334404cf66eb
annett21 <annettarasevich@gmail.com>
2022-06-17 14:01:22 +0300

    resolved 3rd task

--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+from collections import Counter
+
+
+def read_file(file_path):
+    with open(file_path) as files:
+        text = files.read()
+        full_text = text.split(sep="\n")
+    return full_text, text
+
+
+def count_words(text):
+    amount_of_words = 0
+    for line in text:
+        amount_of_words += len(line.split())
+    return amount_of_words
+
+
+def count_letters(text):
+    counter = 0
+    for char in text:
+        if char.isalpha():
+            counter += len(char)
+    return counter
+
+
+def find_unique_letter(text):
+    text = text.lower()
+    new_line = ""
+    for letter in text:
+        if letter.isalpha():
+            new_line += letter
+    c = Counter(new_line)
+    unique_letter = c.most_common()[:-2:-1]
+    return unique_letter[0][0]
+
+
+path = "Module22/02_zen_of_python/zen.txt"
+full_text, text = read_file(path)
+
+print("Количество букв в файле: ", count_letters(text))
+print("Количество слов в файле: ", count_words(full_text))
+print("Количество строк в файле:", len(full_text))
+print("Наиболее редкая буква: ", find_unique_letter(text))
