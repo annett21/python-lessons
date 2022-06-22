8cab710240ec325d7945dcb322b9c1c634e60950
annett21 <annettarasevich@gmail.com>
2022-06-22 16:53:06 +0300

    partly resolved 4th task

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+import os
+
+path = os.path.abspath("Module22")
+dirs = 0
+files = 0
+
+for i_elem in os.listdir(path):
+    new_path = os.path.join(path, i_elem)
+    if os.path.isdir(new_path):
+        files += len(os.listdir(new_path))
+        dirs += 1
+    elif os.path.isfile(new_path):
+        files += 1
+
+
+size = os.path.getsize(path)
+
+print(path)
+print("Размер каталога (в Кб):", size / 1024)
+print("Количество подкаталогов:", dirs)
+print("Количество файлов:", files)
