d20ab28114ca75714cc6762a31d8328e53e09b5f
annett21 <annettarasevich@gmail.com>
2022-06-23 11:27:15 +0300

    small changes 4th task

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -3,17 +3,17 @@ import os
 path = os.path.abspath("Module22")
 dirs = 0
 files = 0
+size = os.path.getsize(path)
 
 for i_elem in os.listdir(path):
     new_path = os.path.join(path, i_elem)
+    size += os.path.getsize(new_path)
     if os.path.isdir(new_path):
         files += len(os.listdir(new_path))
         dirs += 1
     elif os.path.isfile(new_path):
         files += 1
 
-
-size = os.path.getsize(path)
 
 print(path)
 print("Размер каталога (в Кб):", size / 1024)
