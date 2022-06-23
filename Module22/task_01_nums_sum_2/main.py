7ef7b10078b747fcca40f2e82de2463ed380ca48
Dzenis Kapeikin <kopeykindzzesen@gmail.com>
2022-06-23 17:37:33 +0300

    witten some tests for first task

--- Module22/task_01_nums_sum_2/main.py
+++ Module22/task_01_nums_sum_2/main.py
@@ -1,15 +1,23 @@
-def get_numbers_from_file(file_path):
-    with open(file_path, "r") as numbers:
-        return [int(i_num.strip()) for i_num in numbers if i_num.strip()]
+# def get_numbers_from_file(file_path):
+#     with open(file_path, "r") as numbers:
+#         return [int(i_num.strip()) for i_num in numbers if i_num.strip()]
 
 
-def write_sum_to_file(file_path, numbers):
-    with open(file_path, "a") as answer:
-        answer.write(str(sum(numbers)) + "\n")
+# def write_sum_to_file(file_path, numbers):
+#     with open(file_path, "a") as answer:
+#         answer.write(str(sum(numbers)) + "\n")
 
 
-absolute_path = "/Users/anatarasevic/python_lessons/Module22/01_nums_sum_2/"
-answer_path = absolute_path + "answer.txt"
-number_path = absolute_path + "numbers.txt"
+# absolute_path = "/Users/anatarasevic/python_lessons/Module22/01_nums_sum_2/"
+# answer_path = absolute_path + "answer.txt"
+# number_path = absolute_path + "numbers.txt"
 
-write_sum_to_file(answer_path, get_numbers_from_file(number_path))
+# write_sum_to_file(answer_path, get_numbers_from_file(number_path))
+
+
+def read_file(file_path: str) -> str:
+    pass
+
+
+def normalize_file_data(file_data: str) -> list[int]:
+    pass
---
+++ tests/Module22/test_01_nums_sum_2.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest.mock import mock_open, patch
+
+from Module22.task_01_nums_sum_2.main import normalize_file_data, read_file
+
+
+class TestTask01(TestCase):
+    def test_read_file(self):
+        file_path = "some/file/path"
+        read_data = "     2\n\n2\n  2\n         2"
+
+        with patch("builtins.open", mock_open(read_data=read_data)) as m:
+            result = read_file(file_path)
+
+        m.assert_called_once_with(file_path, "r")
+        self.assertEqual(result, read_data)
+
+    def test_normalize_file_data(self):
+        file_data = "     2\n\n2\n  2\n         2"
+        result = normalize_file_data(file_data)
+        self.assertEqual([2,2,2,2], result)
