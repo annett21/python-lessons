85515175317c488db2331e8d5dba613ad296a7d7
annett21 <annettarasevich@gmail.com>
2022-06-23 13:08:37 +0300

    resolved 5th task

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -1,1 +1,47 @@
-# TODO здесь писать код
+import os
+
+
+def make_abs_dir_path(path):
+    return os.path.join("/", *path.split())
+
+
+def make_abs_file_path(dir_path, file_name):
+    return os.path.join(dir_path, file_name + ".txt")
+
+
+def check_path(path):
+    return os.path.exists(path)
+
+
+def save_file(path, text):
+    with open(path, "w") as opened_file:
+        opened_file.write(text)
+
+
+text = input("Введите строку: ")
+
+file_name = input("Введите имя файла: ")
+
+while True:
+    save_place = input(
+        "Куда хотите сохранить документ? Введите последовательность папок"
+        " (через пробел): "
+    )
+    dir_path = make_abs_dir_path(save_place)
+    if check_path(dir_path):
+        break
+    print("Данный путь не существует!")
+
+file_path = make_abs_file_path(dir_path, file_name)
+file_exists = check_path(file_path)
+
+if file_exists:
+    answer = input("Вы действительно хотите перезаписать файл? ")
+    if answer != "да":
+        exit()
+
+save_file(file_path, text)
+if file_exists:
+    print("Файл успешно перезаписан!")
+else:
+    print("Файл успешно сохранён!")
