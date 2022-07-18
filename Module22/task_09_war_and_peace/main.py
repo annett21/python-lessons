52e0d191b8ae51c14c6abe597354e57d04b56a48
Dzenis Kapeikin <kopeykindzzesen@gmail.com>
2022-07-18 13:45:50 +0300

    another solution 9th task

--- Module22/task_09_war_and_peace/main.py
+++ Module22/task_09_war_and_peace/main.py
@@ -3,34 +3,77 @@ from collections import Counter
 from zipfile import ZipFile
 
 
-def open_zip_file(file_path: str) -> None:
+def extract_zip(file_path: str) -> None:
+    dir_path = os.path.dirname(file_path)
+    zip_name = os.path.basename(file_path)
+    file_name = ".".join(zip_name.split(".")[:-1])
+
+    extracted_dir_path = os.path.join(dir_path, file_name)
+
     with ZipFile(file_path, "r") as zip_ref:
-        zip_ref.extractall("Module22/task_09_war_and_peace")
+        zip_ref.extractall(extracted_dir_path)
+
+    return extracted_dir_path
 
 
-zip_path = os.path.abspath("Module22/task_09_war_and_peace/voyna-i-mir.zip")
-open_zip_file(zip_path)
+def analyze_text(file_path: str) -> dict[str, int]:
+    stats: dict[str, int] = {}
 
-text_path = os.path.abspath("Module22/task_09_war_and_peace/voyna-i-mir.txt")
+    with open(file_path, "r") as f:
+        for line in f.read():
+            for char, quantity in Counter(line).items():
+                if char.isalpha():
+                    stats[char] = stats.get(char, 0) + quantity
+
+    all_letters_quantity = sum(stats.values())
+    for char, quantity in stats.items():
+        stats[char] = round(quantity / all_letters_quantity, 6)
+
+    return stats
 
 
-all_letters = {}
+def print_stats(stats: dict[str, float]) -> None:
+    sorted_stats = sorted(stats.items(), key=lambda i: i[1], reverse=True)
+    pretty_stats = (" ".join((char, str(quantity))) for char, quantity in sorted_stats)
+    print(*pretty_stats, sep="\n")
 
-with open(text_path) as f:
-    for line in f:
-        letters_in_line = Counter(line)
-        for letter in letters_in_line:
-            if not letter.isalpha():
-                continue
-            letter_counter = all_letters.get(letter, 0)
-            all_letters[letter] = letter_counter + letters_in_line[letter]
 
-print(
-    *(
-        " ".join((letter, str(counter)))
-        for letter, counter in sorted(
-            list(all_letters.items()), key=lambda i: i[1]
-        )
-    ),
-    sep="\n"
-)
+# def open_zip_file(file_path: str) -> None:
+#     with ZipFile(file_path, "r") as zip_ref:
+#         zip_ref.extractall("Module22/task_09_war_and_peace")
+
+
+# zip_path = os.path.abspath("Module22/task_09_war_and_peace/voyna-i-mir.zip")
+# open_zip_file(zip_path)
+
+# text_path = os.path.abspath("Module22/task_09_war_and_peace/voyna-i-mir.txt")
+
+# all_letters = {}
+
+# with open(text_path) as f:
+#     for line in f:
+#         letters_in_line = Counter(line)
+#         for letter in letters_in_line:
+#             if not letter.isalpha():
+#                 continue
+#             letter_counter = all_letters.get(letter, 0)
+#             all_letters[letter] = letter_counter + letters_in_line[letter]
+
+# print(
+#     *(
+#         " ".join((letter, str(counter)))
+#         for letter, counter in sorted(
+#             list(all_letters.items()), key=lambda i: i[1]
+#         )
+#     ),
+#     sep="\n"
+# )
+
+
+if __name__ == "__main__":
+    zip_path = os.path.abspath("Module22/task_09_war_and_peace/voyna-i-mir.zip")
+    dir_path = extract_zip(zip_path)
+    file_path = os.path.join(dir_path, os.path.basename(dir_path) + ".txt")
+
+    stats = analyze_text(file_path)
+    print_stats(stats)
